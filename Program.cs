e459f46cbf5326b0a1dbf77d4a200c57f045765b
Rahul <rahulsingh191219960@gmail.com>
2020-03-02 23:25:35 +0530

    Almost increasing

--- Program.cs
+++ Program.cs
@@ -6,9 +6,44 @@ namespace CodingProblems
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            ImageRotation im = new ImageRotation();
-            im.ShowArray();
+            // Console.WriteLine("Hello World!");
+            // ImageRotation im = new ImageRotation();
+            // im.ShowArray();
+            int[] arr = { 10,5,6,7,8};
+            bool ifPal = almostIncreasingSequence(arr);
         }
+
+        static bool almostIncreasingSequence(int[] sequence)
+        {
+            if (sequence.Length == 2) { return true; }
+            int countOne = 0;
+            int countTwo = 0;
+            for (int i = 0; i < sequence.Length - 1; i++)
+            {
+                if (sequence[i] >= sequence[i + 1])
+                {
+                    countOne++;
+                }
+                if (i != 0)
+                {
+                    if (sequence[i - 1] >= sequence[i + 1])
+                    {
+                        countTwo++;
+                    }
+                }
+            }
+
+            if (countOne == 1 && countTwo <= 1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+
     }
 }
